b5f41adc9915cbf70f040ba16c25db9356167380
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-11-11 19:41:41 -0300

    init: initial commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap().then(() => Logger.log('🚀 Server started'));
