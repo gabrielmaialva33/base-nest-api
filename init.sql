dc7911e042078b5724534a31cb4ad8c35b1313e5
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-11-12 07:20:46 -0300

    feat: create database migration and prisma service

--- init.sql
+++ init.sql
@@ -4,4 +4,4 @@ CREATE DATABASE pda_app_testing;
 
 GRANT ALL PRIVILEGES ON DATABASE pda_app TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE pda_app_development TO postgres;
-GRANT ALL PRIVILEGES ON DATABASE pda_app_testing TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE pda_app_testing TO pos 
--- prisma/migrations/20221112095428_users/migration.sql
+++ prisma/migrations/20221112095428_users/migration.sql
@@ -6,12 +6,12 @@ CREATE TABLE "users" (
     "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
     "first_name" VARCHAR(80) NOT NULL,
     "last_name" VARCHAR(80) NOT NULL,
-    "full_name" VARCHAR(160)  generated always as (first_name || ' ' || last_name) stored,
+    "full_name" VARCHAR(160) NOT NULL DEFAULT 'concat(first_name, '' '', last_name)',
     "email" VARCHAR(255) NOT NULL,
     "user_name" VARCHAR(50) NOT NULL,
     "is_deleted" BOOLEAN NOT NULL DEFAULT false,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
     "deleted_at" TIMESTAMP(3),
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
