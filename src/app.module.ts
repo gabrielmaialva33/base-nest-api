3697adf7125220eba46ea33b58cfc3de64a094e4
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-12-06 02:52:14 -0300

    feat: implement user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { NestConfigModule } from '@src/lib/config/config.module';
 import { NestOrmModule } from '@src/lib/orm/orm.module';
+import { SharedModule } from '@src/modules/shared.module';
 
 @Module({
-  imports: [NestConfigModule, NestOrmModule],
+  imports: [NestConfigModule, NestOrmModule, SharedModule],
   controllers: [],
   providers: [],
 })
