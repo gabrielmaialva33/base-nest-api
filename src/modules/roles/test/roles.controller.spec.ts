f2e78f4302820c7575db9c77017baba8ace593c5
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-12-21 16:24:10 -0300

    test: ensure all test

--- src/modules/roles/test/roles.controller.spec.ts
+++ src/modules/roles/test/roles.controller.spec.ts
@@ -2,20 +2,36 @@ import { Test, TestingModule } from '@nestjs/testing';
 
 import { RolesController } from '@src/modules/roles/controllers/roles.controller';
 import { RolesService } from '@src/modules/roles/services/roles.service';
+import {
+  IRoleRepository,
+  ROLE_REPOSITORY,
+} from '@src/modules/roles/interfaces/roles.interface';
+import { createMock } from '@golevelup/ts-jest';
 
 describe('RolesController', () => {
   let controller: RolesController;
+  let mockRoleRepository: jest.Mocked<IRoleRepository>;
 
   beforeEach(async () => {
+    jest.clearAllMocks();
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RolesController],
-      providers: [RolesService],
+      providers: [
+        RolesService,
+        {
+          provide: ROLE_REPOSITORY,
+          useValue: createMock<IRoleRepository>(),
+        },
+      ],
     }).compile();
 
     controller = module.get<RolesController>(RolesController);
+    mockRoleRepository = module.get(ROLE_REPOSITORY);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    expect(mockRoleRepository).toBeDefined();
   });
 });
--- src/modules/roles/test/roles.service.spec.ts
+++ src/modules/roles/test/roles.service.spec.ts
@@ -1,19 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { RolesService } from '@src/modules/roles/services/roles.service';
+import {
+  IRoleRepository,
+  ROLE_REPOSITORY,
+} from '@src/modules/roles/interfaces/roles.interface';
+import { createMock } from '@golevelup/ts-jest';
 
 describe('RolesService', () => {
   let service: RolesService;
+  let mockRoleRepository: jest.Mocked<IRoleRepository>;
 
   beforeEach(async () => {
+    jest.clearAllMocks();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RolesService],
+      providers: [
+        RolesService,
+        {
+          provide: ROLE_REPOSITORY,
+          useValue: createMock<IRoleRepository>(),
+        },
+      ],
     }).compile();
 
     service = module.get<RolesService>(RolesService);
+    mockRoleRepository = module.get(ROLE_REPOSITORY);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(mockRoleRepository).toBeDefined();
   });
 });
