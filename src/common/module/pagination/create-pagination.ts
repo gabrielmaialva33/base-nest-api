4393021e27e270254e20761ae9ded1064343ecb2
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-01-13 02:14:19 -0300

    fix: create pagination metadata

--- src/common/module/pagination/create-pagination.ts
+++ src/common/module/pagination/create-pagination.ts
@@ -22,23 +22,23 @@ export function createPagination<T>({
   per_page = 10,
   ...rest
 }: PaginationOptions<T>): Pagination<T> {
-  if (page < 1 || per_page < 1) {
+  if (+page < 1 || +per_page < 1) {
     throw new Error('Page and per_page values must be positive.');
   }
 
-  const totalPages = Math.ceil(total / per_page);
+  const totalPages = Math.ceil(+total / +per_page);
 
   const meta: PaginationMeta = {
-    total,
-    current_page: page,
-    per_page,
-    total_pages: totalPages,
-    first: page > 1 ? 1 : null,
-    previous: page > 1 ? page - 1 : null,
-    next: page < totalPages ? page + 1 : null,
-    last: page < totalPages ? totalPages : null,
-    has_more: page < totalPages,
-    has_previous: page > 1,
+    total: +total,
+    current_page: +page,
+    per_page: +per_page,
+    total_pages: +totalPages,
+    first: +page > 1 ? 1 : null,
+    previous: +page > 1 ? +page - 1 : null,
+    next: +page < +totalPages ? +page + 1 : null,
+    last: +page < +totalPages ? +totalPages : null,
+    has_more: +page < +totalPages,
+    has_previous: +page > 1,
     ...rest,
   };
 
